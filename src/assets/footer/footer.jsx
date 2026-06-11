baa3f9548ba6807e28ed1b8c1762f6567c3f2493
anliyouDev <massomaanliyoufofana@gmail.com>
2026-06-11 05:24:44 +0100

    fix: navbar mobile fermeture menu

--- src/assets/footer/footer.jsx
+++ src/assets/footer/footer.jsx
@@ -26,13 +26,13 @@ function Footer() {
         <a href="https://github.com/anliyouDev" target="_blank" rel="noopener noreferrer" className="social-btn github">
           <IconBrandGithub size={18}/>
         </a>
-        <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer" className="social-btn linkedin">
+        <a href="https://www.linkedin.com/in/massoma-anliyou-fofana-a73871271/" target="_blank" rel="noopener noreferrer" className="social-btn linkedin">
           <IconBrandLinkedin size={18}/>
         </a>
-        <a href="https://www.facebook.com/ton-profil" target="_blank" rel="noopener noreferrer" className="social-btn facebook">
+        <a href="https://www.facebook.com/anliyou.fofana.287438/" target="_blank" rel="noopener noreferrer" className="social-btn facebook">
           <IconBrandFacebook size={18}/>
         </a>
-        <a href="https://wa.me/21658411816" target="_blank" rel="noopener noreferrer" className="social-btn whatsapp">
+        <a href="https://wa.me/21656186345" target="_blank" rel="noopener noreferrer" className="social-btn whatsapp">
           <IconBrandWhatsapp size={18}/>
         </a>
       </div>
